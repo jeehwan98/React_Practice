0443712bf6d971d5073e8c3f672ead05b399d693
Jee Hwan <jeehwan98@naver.com>
2024-07-04 16:34:51 +0800

    Redux configureStore

--- 20_Redux_Project/src/store/index.js
+++ 20_Redux_Project/src/store/index.js
@@ -1,64 +1,13 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './auth.js';
+import counterReducer from './counter.js';
 
-const initialState = { counter: 0, showCounter: true };
-
-createSlice({ // preparing a slice of our global state
-    name: 'counter', // identifier
-    initialState: initialState,
-    reducers: {
-
+// configureStore method: helps us to combine lots of states into one global reducer
+const store = configureStore({
+    reducer: { // store has to have only 1 root reducer
+        counter: counterReducer,
+        auth: authReducer
     }
-})
-
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        };
-    }
-
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        };
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-
-    return state;
-}
-
-// create redux store
-const store = createStore(counterReducer);
+});
 
 export default store; // export so that we can use logic outside of this file
-
-// subscriber function
-const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState);
-}
-
-const incrementAction = { type: 'increment' };
-const decrementAction = { type: 'decrement' };
-
-// subscribe to store changes
-store.subscribe(counterSubscriber);
-
-store.dispatch(incrementAction);
-store.dispatch(decrementAction);
